f0f0a29134430f85b0462c8753b4195804d1f32c
Bruna SaNog <brunasanog.contato@gmail.com>
2023-10-02 13:20:09 -0300

    Agora existem só dois objetos no banco de dados para testes

--- src/bancodedados.js
+++ src/bancodedados.js
@@ -7,62 +7,8 @@ const instrumentos = [
     },
 
     {
-        instrumento: "trompete",
-        quantidade: 1
-
-    },
-
-    {
         instrumento: "guitarra",
-        quantidade: 10
-
-    },
-
-    {
-        instrumento: "teclado",
-        quantidade: 2
-
-    },
-
-    {
-        instrumento: "bateria",
-        quantidade: 1
-
-    },
-
-    {
-        instrumento: "saxofone",
-        quantidade: 2
-
-    },
-
-    {
-        instrumento: "flauta acustica",
-        quantidade: 11
-
-    },
-
-    {
-        instrumento: "flauta transversal",
-        quantidade: 3
-
-    },
-
-    {
-        instrumento: "violoncelo",
-        quantidade: 2
-
-    },
-
-    {
-        instrumento: "baixo",
-        quantidade: 6
-
-    },
-
-    {
-        instrumento: "baixo acustico",
-        quantidade: 1
+        quantidade: 4
 
     },
 
